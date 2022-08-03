1606020ae8a0e5e7702503bc09eb084b7bd020d0
Jeremy White <jcwhite414@gmail.com>
2022-08-02 20:53:51 -0500

    added circle calculations in main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+// Store a radius into a variable.
+// Calculate the circumference based on the radius, and output "The circumference is NN".
+// Calculate the area based on the radius, and output "The area is NN".
+
+const circleRadius = 4;
+
+const circumference = 2 * circleRadius * 3.14;
+
+const area = circleRadius *circleRadius * 3.14;
+
+console.log(`The circumference of the circle is ${circumference}`);
+
+
+console.log(`The area of the circle is ${area}`);
+
